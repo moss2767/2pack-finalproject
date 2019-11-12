d534c0f12f01602f9c70fa7e991737101de6fc0e
Sasha Murray <sasha.murray@appliedtechnology.se>
2019-11-12 15:08:10 +0100

    Simple server socket.io connection

--- server/server.js
+++ server/server.js
@@ -1,13 +1,23 @@
+import cors from 'cors'
 import express from 'express'
+import http from 'http'
+import socket from 'socket.io'
 
 const app = express()
-
+const server = http.Server(app)
+const io = socket(server)
 const port = process.env.PORT || 8000
+
+app.use(cors())
 
 app.get('/', (req, res) => {
   res.send('hello')
 })
 
-app.listen(port, () => {
+io.on('connection', socket => {
+  io.emit('test', "Hello World!")
+})
+
+server.listen(port, () => {
   console.log(`Listening on port ${port}...`)
 })
