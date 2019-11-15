59f973143d81214fcbb5128b257eec1eff23317d
Axel Eriksson <axeleriksson0@gmail.com>
2019-11-15 12:56:09 +0100

    Redux middleware

--- client/src/index.js
+++ client/src/index.js
@@ -4,13 +4,32 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import { Provider } from 'react-redux'
-import { createStore } from 'redux'
+import { createStore, applyMiddleware, compose } from 'redux'
 import rootReducer from './reducers/rootReducer'
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const testMiddleware = ({ getState }) => {
+  return next => action => {
+    console.log('will dispatch', action)
+
+    // Call the next dispatch method in the middleware chain.
+    const returnValue = next(action)
+
+    console.log('state after dispatch', getState())
+
+    // This will likely be the action itself, unless
+    // a middleware further in chain changed it.
+    return returnValue
+  }
+} 
 
 const store = createStore(
   rootReducer,
-  undefined,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  {},
+  composeEnhancers(
+    applyMiddleware(testMiddleware)
+  )
 )
 
 ReactDOM.render(
