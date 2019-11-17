2779ad1a6f61360bde6e717dd5dfb720d8dbea4b
Axel Eriksson <axeleriksson0@gmail.com>
2019-11-17 19:09:45 +0100

    Refactored NavBar

---
+++ client/src/components/NavBar/Style.js
@@ -0,0 +1,23 @@
+import { makeStyles } from '@material-ui/core'
+
+const useStyles = makeStyles(() => ({
+  menuButton: {
+    marginRight: 2,
+  },
+  title: {
+    '&:hover': {
+      cursor: 'pointer',
+    }
+  },
+  rightSide: {
+    marginLeft: "auto",
+  },
+  list: {
+    width: 250,
+  },
+  fullList: {
+    width: 'auto',
+  },
+}))
+
+export default useStyles
