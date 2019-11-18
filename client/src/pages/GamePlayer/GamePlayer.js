b7d027341be98fa58954c499ea786117f9482dc5
Axel Eriksson <axeleriksson0@gmail.com>
2019-11-18 11:50:38 +0100

    separated host and player components

--- client/src/pages/GamePlayer/GamePlayer.js
+++ client/src/pages/GamePlayer/GamePlayer.js
@@ -1,8 +1,39 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { Typography } from '@material-ui/core'
+import Question from '../../components/Question/Question'
+import { useStyles } from './GamePlayerStyle'
 
 const GamePlayer = () => {
+  const classes = useStyles()
+  const [ gameStarted, setGameStarted ] = useState(false)
+  const [ showCancel, setShowCancel ] = useState(false)
+  const [ countdown, setCountdown ] = useState(3)
+
   return (
-    <div>Waiting for host...</div>
+    <div>
+      <Typography className={classes.header} variant="h4">
+        Waiting for host...
+        </Typography> 
+
+
+    { !gameStarted && showCancel && (
+        <div>
+          <Typography className={classes.header} variant="h4">
+           {countdown}...
+          </Typography>
+        </div>
+      )}
+{/* 
+      { gameStarted && (
+        currentQuestion !== questions.length && (
+          <div>
+          <Question question={questions[currentQuestion]}/>
+          <p>Points: {points}</p>
+          </div>
+        ) 
+      )} */}
+
+    </div>
   )
 }
 
---
+++ client/src/pages/GamePlayer/GamePlayerStyle.js
@@ -0,0 +1,16 @@
+import { makeStyles } from '@material-ui/core'
+
+export const useStyles = makeStyles(() => ({
+  header: {
+    fontWeight: "lighter",
+    margin: "1rem 0"
+  },
+  button: {
+    margin: "1rem",
+    padding: "1rem",
+    fontSize: "2rem",
+  },
+  connectedPlayersHeader: {
+    marginBottom: "0"
+  }
+}))
