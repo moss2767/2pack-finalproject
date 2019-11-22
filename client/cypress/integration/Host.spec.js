31983269e78c7f4a493b30f8486e5c68b7092458
Axel Eriksson <axeleriksson0@gmail.com>
2019-11-22 11:36:56 +0100

    First proper test working

--- client/cypress/integration/Host.spec.js
+++ client/cypress/integration/Host.spec.js
@@ -2,5 +2,19 @@ describe('Host', () => {
   it('Hosts a game --> plays through it --> renders result page', () => {
     cy.visit('http://localhost:3000/')
     cy.get('#hostButton').click()
+    cy.get(':nth-child(1) > .MuiButtonBase-root > .MuiCardMedia-root').click()
+    cy.get('#startGame').click()
+
+    cy.get('body').then(body => {
+      const clickNextQuestion = () => {
+        if (body.find('#nextQuestion').length > 0) {
+          cy.get('#nextQuestion').click()
+          clickNextQuestion()
+        }
+      }
+      clickNextQuestion()
+      cy.get('#showResults').click()
+      cy.url().should('eq', 'http://localhost:3000/result')
+    })
   })
 })
